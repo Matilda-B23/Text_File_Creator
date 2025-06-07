b84a2e874d79bb3cdec3d78b2a147158c033b2e2
Matilda-B23 <matilda.bowling16@gmail.com>
2025-06-06 22:08:30 -0400

    Update file_creator.py

--- file_creator.py
+++ file_creator.py
@@ -2,4 +2,4 @@ name = input("What's your name? \n")
 color = input("What's your favorite color? \n")
 with open('example.txt', 'w') as file:
     file.write(f"{name} created this file. ")
-    file.write(f"{color} is her favorite color.")
+    file.write(f"{color} is their favorite color.")
